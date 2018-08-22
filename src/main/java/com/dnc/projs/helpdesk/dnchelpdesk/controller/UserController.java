9e98c388aa69af2a370b72368aa362c5f813d4a0
Denis Caminha <denis.caminha@geosales.com.br>
2018-08-22 13:21:48 -0300

    cria delete e findall metodos no usercontroller

--- src/main/java/com/dnc/projs/helpdesk/dnchelpdesk/controller/UserController.java
+++ src/main/java/com/dnc/projs/helpdesk/dnchelpdesk/controller/UserController.java
@@ -5,6 +5,7 @@ import com.dnc.projs.helpdesk.dnchelpdesk.service.UserService;
 import com.dnc.projs.helpdesk.dnchelpdesk.util.Response;
 import com.mongodb.DuplicateKeyException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -64,25 +66,50 @@ public class UserController {
 			user.setPassword(passwordEncoder.encode(user.getPassword()));
 			this.userService.createOrUpdate(user);
 			response.setData(user);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			response.getErrors().add(e.getMessage());
 		}
 		return ResponseEntity.ok(response);
 	}
 
-	@GetMapping(value="{id}")
+	@GetMapping(value = "{id}")
 	public ResponseEntity<Response<User>> findById(@PathVariable("id") String id) {
 
 		Response<User> response = new Response<>();
 		Optional<User> optionalUser = this.userService.findById(id);
-		if(!optionalUser.isPresent()) {
+		if (!optionalUser.isPresent()) {
 			response.getErrors().add("Register not found");
 			return ResponseEntity.badRequest().body(response);
 		}
 		response.setData(optionalUser.get());
 		return ResponseEntity.ok(response);
-
 	}
+
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") String userId) {
+		Response<String> response = new Response<>();
+		Optional<User> optionalUser = this.userService.findById(userId);
+
+		if (!optionalUser.isPresent()) {
+			response.getErrors().add("Register not found");
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		this.userService.delete(userId);
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping(value = "{page}/{count}")
+	public ResponseEntity<Response<Page<User>>> findAll(@PathVariable("page") int page,
+														@PathVariable("count") int count) {
+
+		Response<Page<User>> response = new Response<>();
+
+		Page<User> users = this.userService.findAll(page, count);
+		response.setData(users);
+		return ResponseEntity.ok(response);
+	}
+
 
 	private void validateUpdateUser(User user, BindingResult bindingResult) {
 		if (user.getId() == null) {
@@ -92,7 +119,6 @@ public class UserController {
 			bindingResult.addError(new ObjectError("User", "Email no information"));
 		}
 	}
-
 
 	private void validateCreateUser(User user, BindingResult bindingResult) {
 		if (user.getEmail() == null) {
